c6b37a3529679cde9ec6da7ad8b8a5b621e6b18d
Já <email@jendakolda.cz>
2020-09-29 09:09:24 +0200

    pokus playground, dearpygui 0.4.0 implemented

--- modules/gui.py
+++ modules/gui.py
@@ -1,6 +1,21 @@
 from dearpygui.core import *
 from dearpygui.simple import *
 
+tab2_input_txt = [
+    "##valve_tag",
+    "kg / s, Mass flow rate of steam through the valve.##2widget1",
+    "K, Temperature of steam entering the valve.##2widget2",
+    "Pa, Upstream relieving pressure; set pressure + allowable overpressure + atm. pressure.##2widget3",
+    "[-], (Optional, default = 0.975) The effective discharge coefficient.##2widget4",
+    "[-], (Optional, default = 1) Correction due to vapor backpressure.##2widget5",
+    "[-], (Optional, default = 1) Combination correction factor"
+    " for installation with a rupture disk upstream of the PRV,##2widget6",
+]
+tab2_input_def = ['PSV', 0, 273.15, 101325, 0.975, 1, 1]
+
+
+# tab2_input_val = []
+
 
 def theme_setting(sender, data):
     set_theme(data)
@@ -14,6 +29,16 @@ def formula_show(sender, data):
 
 def on_formula_close(sender, data):
     delete_item(sender)
+
+
+def clear_fields(sender, data):
+    for i in range(len(tab2_input_txt)):
+        set_value(tab2_input_txt[i], tab2_input_def[i])
+
+
+def retrieve_values(sender, data):
+    tab2_input_val = [get_data(tab2_input_txt[i]) for i, txt in enumerate(tab2_input_txt)]
+    print(tab2_input_val)
 
 
 class GuiBuilder:
@@ -44,32 +69,16 @@ class Menu:
 
 
 class Tabs:
-    with tab_bar(name='tab_bar_1'):
-        pass
+    add_tab_bar(name='tab_bar_1')
 
 
 class Steam:
-    @staticmethod
-    def retrieve_values(sender, data):
-        pass
-
-    @staticmethod
-    def clear_fields(sender, data):
-        for i in data:
-            set_value(i, 0)
 
     @staticmethod
     def export_results(sender, data):
         pass
 
     with tab(name='Steam##tab2', parent='tab_bar_1'):
-        # lists of data on tab 2:
-        tab2_inputs = ["##valve_tag",
-                       "kg / s, Mass flow rate of steam through the valve.##2widget1",
-                       "K, Temperature of steam entering the valve.##2widget2",
-                       "Pa, Upstream relieving pressure; set pressure + allowable overpressure + atm. pressure.##2widget3",
-                       ]
-
         with group('2heading'):
             add_spacing(count=2)
             add_text(
@@ -79,19 +88,21 @@ class Steam:
             add_indent(offset=20)
             add_button("Show Formula", callback=formula_show)
             add_same_line(spacing=20)
-            add_button("Clear All", callback=clear_fields, callback_data=tab2_inputs)
+            add_button("Clear All", callback=clear_fields, callback_data=tab2_input_txt)
             add_same_line(spacing=20)
             add_button("Export Results", callback=export_results)
             unindent()
             add_spacing(count=2)
             add_separator()
+
         with group('##2input_widgets', tip='Input data for Steam relieve calculation'):
             add_spacing(count=5)
 
             add_indent(offset=20)
             add_text('Valve Tag')
             add_same_line(spacing=10)
-            add_input_text(tab2_inputs[0], default_value='PSV', uppercase=True, no_spaces=True, width=100,
+            add_input_text(tab2_input_txt[0], default_value=tab2_input_def[0], uppercase=True, no_spaces=True,
+                           width=100,
                            tip='Name of the valve')
             add_spacing(count=5)
             unindent()
@@ -99,42 +110,36 @@ class Steam:
             add_indent(offset=5)
             add_text('m  =')
             add_same_line(spacing=10)
-            add_input_float(tab2_inputs[1], default_value=0,
-                            width=75, tip='Zadej cislo',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[1], default_value=tab2_input_def[1],
+                            width=75, tip='Zadej cislo', on_enter=True)
 
             add_text('T  =')
             add_same_line(spacing=10)
-            add_input_float(tab2_inputs[2],
-                            default_value=273.15, width=75, tip='Zadej cislo',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[2],
+                            default_value=tab2_input_def[2], width=75, tip='Zadej cislo', on_enter=True)
 
             add_text('P1 =')
             add_same_line(spacing=10)
-            add_input_float(tab2_inputs[3],
-                            default_value=101325, width=75, tip='Input pressure [Pa]',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[3],
+                            default_value=tab2_input_def[3], width=75, tip='Input pressure [Pa]', on_enter=True)
 
             add_text('Kd =')
             add_same_line(spacing=10)
-            add_input_float("[-], (Optional, default = 0.975) The effective discharge coefficient.##2widget4",
-                            default_value=0.975, width=75, tip='Kd = (default = 0.975)',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[4],
+                            default_value=tab2_input_def[4], width=75, tip='Kd = (default = 0.975)', on_enter=True)
 
             add_text('Kb =')
             add_same_line(spacing=10)
-            add_input_float("[-], (Optional, default = 1) Correction due to vapor backpressure.##2widget5",
-                            default_value=1, width=75, tip='Kb = (default = 1)',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[5],
+                            default_value=tab2_input_def[5], width=75, tip='Kb = (default = 1)', on_enter=True)
 
             add_text('Kc =')
             add_same_line(spacing=10)
-            add_input_float("[-], (Optional, default = 1) Combination correction factor"
-                            " for installation with a rupture disk upstream of the PRV,##2widget6",
-                            default_value=1, width=75, tip='Kc = (default = 1)',
-                            callback=retrieve_values, on_enter=True)
+            add_input_float(tab2_input_txt[6],
+                            default_value=tab2_input_def[6], width=75, tip='Kc = (default = 1)', on_enter=True)
             unindent()
             add_separator()
+
         with group('##2output_widgets', tip='Output data for Steam relieve calculation'):
             add_spacing(count=5)
 
@@ -144,6 +149,7 @@ class Steam:
             # add_input_text("##valve_tag", uppercase=True, no_spaces=True, width=100)
             add_spacing(count=5)
             unindent()
+            add_button('Calculate!', callback=retrieve_values)
 
     # with tab(name='Gas/Vapor##tab1'):
     #     add_text('tohle je tab 1')
